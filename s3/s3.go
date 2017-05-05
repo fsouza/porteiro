df1a1cf3a41540a9bfd8127eefa5961c1ef57af5
Francisco Souza <fsouza@users.noreply.github.com>
2017-05-04 23:43:52 -0400

    s3: add a brief package description

--- s3/s3.go
+++ s3/s3.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package s3 provides an opener for S3 resources declared in the format
+// s3://<bucket-name>/<object-key>.
 package s3
 
 import (
